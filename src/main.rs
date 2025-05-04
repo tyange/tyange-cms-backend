b4814a6d88e868ac223dc175790a6ce368bea9b8
유태양 <taeyang.yu@autowini.com>
2025-05-04 22:03:47 +0900

    Auth middleware는 특정 라우트에만 작동해야 합니다

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ pub struct AppState {
 
 fn configure_routes() -> Route {
     Route::new()
-        .at("/upload-post", poem::post(upload_post))
+        .at("/upload-post", poem::post(upload_post).with(Auth))
         .at("/login", poem::post(login))
 }
 
@@ -45,16 +45,13 @@ async fn main() -> Result<(), std::io::Error> {
         upload_dir: PathBuf::from("./uploads"),
     });
 
-    let app = configure_routes()
-        .data(state)
-        .with(
-            Cors::new()
-                .allow_origin("http://localhost:3000")
-                .allow_methods(vec!["GET", "POST"])
-                .allow_credentials(true)
-                .allow_headers(vec!["content-type"]),
-        )
-        .with(Auth);
+    let app = configure_routes().data(state).with(
+        Cors::new()
+            .allow_origin("http://localhost:3000")
+            .allow_methods(vec!["GET", "POST"])
+            .allow_credentials(true)
+            .allow_headers(vec!["content-type"]),
+    );
 
     Server::new(TcpListener::bind("0.0.0.0:8080"))
         .run(app)
